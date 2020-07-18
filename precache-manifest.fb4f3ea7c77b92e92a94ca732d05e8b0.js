d6fb15c31d59df2cb535b7297f6c73f06bd5a081
UsamaSaleem148 <usamasaleem674@gmail.com>
2020-07-18 21:44:15 +0500

    Updates

---
+++ precache-manifest.fb4f3ea7c77b92e92a94ca732d05e8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd949dc650c7951b494e3fa28eb58e26",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9632fe9e210a9762ec31",
+    "url": "/todolist/static/css/main.407879c1.chunk.css"
+  },
+  {
+    "revision": "67b7a2d446f1fd4b0bc2",
+    "url": "/todolist/static/js/2.73333b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.73333b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9632fe9e210a9762ec31",
+    "url": "/todolist/static/js/main.6727fec8.chunk.js"
+  },
+  {
+    "revision": "b97d7cb31fa7e5a5931e",
+    "url": "/todolist/static/js/runtime-main.811a1a38.js"
+  }
+]);
